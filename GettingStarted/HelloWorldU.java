1659dc41ff8ab40393a893f724b191b6aab1db6f
uttamSinha <uttamkumar.sinha95@gmail.com>
2019-04-12 01:36:01 +0530

    Second commit via intelli -- trying to avoid commiting unnecessary files

---
+++ GettingStarted/HelloWorldU.java
@@ -0,0 +1,7 @@
+public class HelloWorldU {
+public static void main(String args[])
+{
+    System.out.println("Second file via intellij");
+    System.out.println("Trying to avoid pushing unncessary files");
+}
+}
